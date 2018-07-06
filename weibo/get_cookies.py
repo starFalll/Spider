e6dab7b665556226136fe2d9cd6c34e890e16986
starFalll <fyxformal@gmail.com>
2018-07-06 17:02:01 +0800

    change web site to prevent 403

---
+++ weibo/get_cookies.py
@@ -0,0 +1,46 @@
+import pickle
+from selenium import webdriver
+import time
+from weibo.Connect_mysql import Connect
+
+
+def get():
+    conf, engine = Connect('conf.yaml')  # 获取配置文件的内容
+    loginname = conf.get('loginname')
+    password = conf.get('password')
+
+    loginname = list(loginname.values())
+    password = list(password.values())
+    with open('cookies.pkl', 'wb') as f:
+        for i in range(len(password)):  # 将每个账号的cookies保存下来.
+            try:
+                driver = webdriver.Chrome()
+                driver.set_window_size(1124, 850)  # 防止得到的WebElement的状态is_displayed为False，即不可见
+                driver.get("http://www.weibo.com/login.php")
+                time.sleep(5)
+                driver.find_element_by_xpath('//*[@id="loginname"]').clear()
+                driver.find_element_by_xpath('//*[@id="loginname"]').send_keys(loginname[i])
+                driver.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[2]/div/input').clear()
+
+                time.sleep(2)
+                driver.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[2]/div/input').send_keys(
+                    password[i])
+                driver.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[6]/a').click()
+                driver.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[3]/div/input').send_keys(
+                    input("输入验证码： "))
+
+                time.sleep(1)
+                driver.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[6]/a').click()
+            except Exception as e:
+                print("验证码输入错误,请重新输入!")
+                driver.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[3]/div/input').send_keys(
+                    input("输入验证码： "))
+                time.sleep(1)
+                driver.find_element_by_xpath('//*[@id="pl_login_form"]/div/div[3]/div[6]/a').click()
+            cookies = driver.get_cookies()
+            print(cookies)
+            pickle.dump(cookies, f)
+
+
+if __name__ == '__main__':
+    get()
