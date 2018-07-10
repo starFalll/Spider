0124dfb9b54e05e2dc8f6946418a167c76d4df23
starFalll <fyxformal@gmail.com>
2018-07-10 14:14:24 +0800

    fix time difference bug

--- weibo/weibo_spider.py
+++ weibo/weibo_spider.py
@@ -7,23 +7,28 @@ import re
 import time
 import random
 import pymysql
-
+import datetime
 
 
 def getinfo(conn, driver, uid, table):
     time.sleep(2)
     driver.get('https://weibo.com/p/100505' + str(uid) + '/info?mod=pedit_more')  # 资料页
-    time.sleep(2)
-    html = driver.page_source
+    time.sleep(5)
+
+
+    '''
+    正则表达式组
+    '''
     info = re.compile(r'<span class="pt_title S_txt2"(.*?)</li>', re.S)  # 信息
-    information = re.compile(r'>(.*?)：', re.S)     # 匹配信息名称字段
+    information = re.compile(r'>(.*?)：', re.S)  # 匹配信息名称字段
     content = re.compile(r'class="pt_detail">(.*?)</span>', re.S)  # 内容
     job = re.compile(r'loc=infjob" target="_blank">(.*?)</a>', re.S)  # 工作信息
     edu = re.compile(r'loc=infedu">(.*?)</a>', re.S)  # 教育信息
     certif = re.compile(r'class="pf_intro" title="(.*?)">', re.S)  # 认证信息
 
+    html = driver.page_source
     infos = re.findall(info, html)
-    if(len(infos)==0):
+    if (len(infos) == 0):
         driver.get('https://weibo.com/p/103505' + str(uid) + '/info?mod=pedit_more')  # 大V资料页
         time.sleep(2)
         html = driver.page_source
@@ -41,8 +46,9 @@ def getinfo(conn, driver, uid, table):
     Education_info = ''
     Work_info = ''
     Description = ''
+    for Cert in cer:
+        Certified += Cert
 
-    Certified += cer[0]
     for job in jobs:
         Work_info += job + ' '
     for edu in edus:
@@ -92,12 +98,14 @@ def execute_times(driver, times):
     t = re.compile(r'<span class="time">(.*?)<', re.S)  # 匹配动态发布时间
     for i in range(times + 1):
         try:
-            print(str(i/times*100)+'%')
+            print(str(i / times * 100) + '%')
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # 往下滑动一次
             time.sleep(random.random())
         except:
             break
     html = driver.page_source
+    with open('test.html','w') as f:
+        f.write(html)
     dynamic += re.findall(d, html)
     T += re.findall(t, html)
     return dynamic, T
@@ -106,11 +114,30 @@ def execute_times(driver, times):
 def getmain(cookies, uid, conn, table_data, table_user):
     driver = webdriver.Chrome()
     driver.get("http://weibo.com")
+    time.sleep(3)
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    driver.refresh()
+
+    getinfo(conn, driver, uid, table_user)#获取资料页
+    time.sleep(2)
+    driver.get("https://m.weibo.cn/p/100505" + str(uid) + "#feedtop")
+    time.sleep(2)
+    dynamics, times = execute_times(driver, 300)
+    time.sleep(2)
+    driver.close()
+
+    '''
+    正则表达式组
+    '''
     re_nbsp = re.compile(r'&nbsp', re.S)  # 去除$nbsp
     re_html = re.compile(r'</?\w+[^>]*>', re.S)  # 去除html标签
     re_200b = re.compile(r'\u200b', re.S)  # 去除分隔符
     re_quot = re.compile(r'&quot', re.S)
-    emoji_pattern = re.compile(
+    today_time = re.compile(r'\d+小时前', re.S)
+    yesterday_time = re.compile(r'昨天', re.S)
+    recent_time = re.compile(r'^\d{2}-\d{2}$', re.S)
+    emoji_pattern = re.compile(#去除表情,有些超过了'utf8mb4'编码范围,暂时不把表情存入数据库
         u"(\ud83d[\ude00-\ude4f])|"  # emoticons
         u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
         u"(\ud83d[\u0000-\uddff])|"  # symbols & pictographs (2 of 2)
@@ -118,16 +145,25 @@ def getmain(cookies, uid, conn, table_data, table_user):
         u"(\ud83c[\udde0-\uddff])|"  # flags (iOS)
         u"[\U00010000-\U0010ffff]|[\uD800-\uDBFF][\uDC00-\uDFFF]"
         "+", re.S)
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-    driver.refresh()
-    getinfo(conn, driver, uid, table_user)
-    time.sleep(2)
-    driver.get("https://m.weibo.cn/p/100505" + str(uid) + "#feedtop")
-    time.sleep(2)
-    dynamics, times = execute_times(driver, 20)
-    time.sleep(2)
+
+    strtoday = datetime.date.today()
+    stryesterday = strtoday - datetime.timedelta(days=1)
+    today = strtoday.strftime('%m月%d日')
+    yesterday = stryesterday.strftime('%m月%d日')
+
     for i in range(len(times)):
+        # 将时间中的今天(**小时前),昨天(昨天 **)和最近(month-day)这三特殊情况转化为'month月day日'格式
+        istoday = re.findall(today_time, times[i])
+        isyesterday = re.findall(yesterday_time, times[i])
+        isrecenttime = re.findall(recent_time, times[i])
+
+        if (istoday):  # 列表不为空
+            times[i] = today + ' ' + str(i)
+        elif (isyesterday):
+            times[i] = yesterday + ' ' + str(i)
+        elif (isrecenttime):
+            times[i] += '日'
+            times[i] = times[i].replace('-', '月') + ' ' + str(i)
         dynamics[i] = dynamics[i].strip()
         dynamics[i] = re_nbsp.sub('', dynamics[i])
         dynamics[i] = re_html.sub('', dynamics[i])
@@ -137,7 +173,7 @@ def getmain(cookies, uid, conn, table_data, table_user):
         ins = insert(table_data).values(uid=uid, weibo_cont=pymysql.escape_string(dynamics[i]), create_time=times[i])
         ins = ins.on_duplicate_key_update(weibo_cont=pymysql.escape_string(dynamics[i]))
         conn.execute(ins)
-    driver.close()
+    print('end------------------------------------------')
 
 
 def main():
@@ -152,6 +188,7 @@ def main():
     for uid in uids:
         getmain(cookies, uid, conn, wb_data, wb_user)
     conn.close()
+
 
 if __name__ == '__main__':
     main()
